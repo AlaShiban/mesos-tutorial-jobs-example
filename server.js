1a5f85a06947a803f3626e85887bdea41a2a1e42
Ala Shiban <a.shiban@gmail.com>
2016-10-09 17:44:42 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var request = require('request');
+var process = require('process')
+var url = process.env.POSTURL;
+
+request.post(url, {
+    form: {
+        randomNumber: '' + getRandomInt(0, 10000)
+    }
+}, function (error, response, body) {
+    if (!error) {
+        console.log(body);
+    } else {
+        console.log(error);
+    }
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
